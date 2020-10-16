c49264b806f80839757bc1a7e4b795e08618d07d
tonyw0527 <tonyw0527@gmail.com>
2020-10-16 19:28:40 +0900

    Updated painter component.

--- src/components/painter/Painter.js
+++ src/components/painter/Painter.js
@@ -1,141 +1,95 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './Painter.css';
 
 const Painter = () => {
 
-    // canvas width, height, context
-    const [W, setW] = useState(0);
-    const [H, setH] = useState(0);
-    const [Ctx, setCtx] = useState();
+    const [Eraser_flag, setEraser_flag] = useState(false);
 
-    const painterRef = useRef(null); // App component ref
-    const canvasRef = useRef(null);
+    const myCanvasRef = useRef()
 
     useEffect(() => {
+        const myCanvas = myCanvasRef.current;
+        const myCtx = myCanvas.getContext('2d');
+        if(Eraser_flag === false) {
+            myCtx.fillStyle = 'black';
+            myCtx.strokeStyle = 'black';
+            myCtx.lineWidth = 3;
+        } else {
+            myCtx.fillStyle = 'white';
+            myCtx.strokeStyle = 'white';
+            myCtx.lineWidth = 15;
+        }
+        
+        const rectWidth = 3;
+        myCtx.lineJoin = 'round';
+        myCtx.lienCap = 'round';
+
         let prevX = 0;
-        let currX = 0;
         let prevY = 0;
+        let currX = 0;
         let currY = 0;
-        let flag = false;
-        let dot_flag = false;
+        const forXCorrection = myCanvas.getBoundingClientRect().left;
+        const forYCorrection = myCanvas.getBoundingClientRect().top;
 
-        let color = "black"
-        let y = 3;
-        
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-        setCtx(ctx);
-        setW(canvas.width);
-        setH(canvas.height);
+        let path_flag = false;
+        const drawing = (type, e) => {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - forXCorrection;
+            currY = e.clientY- forYCorrection;
 
-        canvas.addEventListener('mousemove', e => {
-            findxy('move', e);
-        }, false);
-        canvas.addEventListener("mousedown", function (e) {
-            findxy('down', e)
-        }, false);
-        canvas.addEventListener("mouseup", function (e) {
-            findxy('up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function (e) {
-            findxy('out', e)
-        }, false);
-
-
-        // Set up touch events for mobile, etc
-        canvas.addEventListener("touchstart", function (e) {
-            // mousePos = getTouchPos(canvas, e);
-            var touch = e.touches[0];
-            var mouseEvent = new MouseEvent("mousedown", {
-            clientX: touch.clientX,
-            clientY: touch.clientY
-            });
-            canvas.dispatchEvent(mouseEvent);
-        }, false);
-        
-        canvas.addEventListener("touchend", function (e) {
-            var mouseEvent = new MouseEvent("mouseup", {});
-            canvas.dispatchEvent(mouseEvent);
-        }, false);
-        
-        canvas.addEventListener("touchmove", function (e) {
-            var touch = e.touches[0];
-            var mouseEvent = new MouseEvent("mousemove", {
-            clientX: touch.clientX,
-            clientY: touch.clientY
-            });
-            canvas.dispatchEvent(mouseEvent);
-        }, false);
-
-        // Prevent scrolling when touching the canvas
-        painterRef.current.addEventListener("touchstart", function (e) {
-            if (e.target === canvas) {
-            e.preventDefault();
-            }
-        }, false);
-        painterRef.current.addEventListener("touchend", function (e) {
-            if (e.target === canvas) {
-            e.preventDefault();
-            }
-        }, false);
-        painterRef.current.addEventListener("touchmove", function (e) {
-            if (e.target === canvas) {
-            e.preventDefault();
-            }
-        }, false);
-
-        
-        function draw() {
-            ctx.beginPath();
-            ctx.moveTo(prevX, prevY);
-            ctx.lineTo(currX, currY);
-            ctx.strokeStyle = color;
-            ctx.lineWidth = y;
-            ctx.stroke();
-            ctx.closePath();
-        }
-
-        function findxy(res, e) {
-            if (res === 'down') {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.getBoundingClientRect().left;
-                currY = e.clientY - canvas.getBoundingClientRect().top;
-        
-                flag = true;
-                dot_flag = true;
-                if (dot_flag) {
-                    ctx.beginPath();
-                    ctx.fillStyle = color;
-                    ctx.fillRect(currX, currY, 2, 2);
-                    ctx.closePath();
-                    dot_flag = false;
-                }
-            }
-            if (res === 'up' || res === "out") {
-                flag = false;
-            }
-            if (res === 'move') {
-                if (flag) {
-                    prevX = currX;
-                    prevY = currY;
-                    currX = e.clientX - canvas.getBoundingClientRect().left;
-                    currY = e.clientY - canvas.getBoundingClientRect().top;
-                    draw();
-                }
+            if (type === 'down') {
+                path_flag = true;
+                myCtx.fillRect(currX, currY, rectWidth, rectWidth);
+            } else  if (type === 'move' && path_flag) {
+                myCtx.beginPath();
+                myCtx.moveTo(prevX, prevY);
+                myCtx.lineTo(currX, currY);
+                myCtx.stroke();
+                myCtx.closePath();
+            } else if (type === 'up' || type === 'out') {
+                path_flag = false;
             }
         }
-    }, []);
 
-    function erase() {
-        Ctx.clearRect(0, 0, W, H);
-    }
-    
+        const mousedownListener = (e) => {
+            drawing('down', e);
+        }
+
+        const mousemoveListener = (e) => {
+            drawing('move', e);
+        }
+
+        const mouseupListener = (e) => {
+            drawing('up', e);
+        }
+
+        const mouseoutListener = (e) => {
+            drawing('out', e);
+        }
+
+        myCanvas.addEventListener('mousedown', mousedownListener);
+        myCanvas.addEventListener('mousemove', mousemoveListener);
+        myCanvas.addEventListener('mouseup', mouseupListener);
+        myCanvas.addEventListener('mouseout', mouseoutListener);
+
+        return () => {
+            myCanvas.removeEventListener('mousedown', mousedownListener);
+            myCanvas.removeEventListener('mousemove', mousemoveListener);
+            myCanvas.removeEventListener('mouseup', mouseupListener);
+            myCanvas.removeEventListener('mouseout', mouseoutListener);
+        }
+    }, [Eraser_flag])
 
     return (
-        <div className="painter-wrapper" ref={painterRef}>
-            <canvas id="myCanvas" width="300px" height="400px" ref={canvasRef}></canvas>
-            <button type="button" onClick={erase}>ERASE</button>
+        <div className="painter-wrapper">
+        <canvas ref={myCanvasRef} id="myCanvas" width="300" height="300">Error</canvas>
+        <button type="button" onClick={() => {
+            myCanvasRef.current.getContext('2d').clearRect(0,0,myCanvasRef.current.width,myCanvasRef.current.height);
+        }}>Reset</button>
+        <button type="button" onClick={() => {
+            Eraser_flag ? setEraser_flag(false) : setEraser_flag(true);
+        }}>{Eraser_flag ? 'Pen' : 'Eraser'}</button>
         </div>
     );
 }
